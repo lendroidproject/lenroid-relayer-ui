e1873982838432ca095e4d24e242b965c15fdace
Charn Lee <crazy.dev@aol.com>
2018-02-09 12:03:38 -0800

    Add utils for token mapping

--- src/containers/ViewOrders/ViewOrders.js
+++ src/containers/ViewOrders/ViewOrders.js
@@ -3,6 +3,7 @@ import { Col, Table } from 'reactstrap';
 import { Button } from 'antd';
 import { default as Web3 } from 'web3';
 import axios from 'axios';
+import { getTokenNameFromAddress } from '../../utils';
 import './ViewOrders.css';
 
 const orders = [
@@ -10,8 +11,8 @@ const orders = [
     "exchangeContractAddress": "0x12459c951127e0c374ff9105dda097662a027093",
     "maker": "0x9e56625509c2f60af937f23b7b532600390e8c8b",
     "taker": "0xa2b31dacf30a9c50ca473337c01d8a201ae33e32",
-    "makerTokenAddress": "0x323b5d4c32345ced77393b3530b1eed0f346429d",
-    "takerTokenAddress": "0xef7fff64389b814a946f3e92105513705ca6b990",
+    "makerTokenAddress": "0xcc2704ce33089d0f051eb0aff1750bb99fdfab46",
+    "takerTokenAddress": "0xd0a1e359811322d97991e03f863a0c30c2cf029c",
     "feeRecipient": "0xb046140686d052fff581f63f8136cce132e857da",
     "makerTokenAmount": "10000000000000000",
     "takerTokenAmount": "20000000000000000",
@@ -25,8 +26,8 @@ const orders = [
     "exchangeContractAddress": "0x12459c951127e0c374ff9105dda097662a027093",
     "maker": "0x9e56625509c2f60af937f23b7b532600390e8c8b",
     "taker": "0xa2b31dacf30a9c50ca473337c01d8a201ae33e32",
-    "makerTokenAddress": "0x323b5d4c32345ced77393b3530b1eed0f346429d",
-    "takerTokenAddress": "0xef7fff64389b814a946f3e92105513705ca6b990",
+    "makerTokenAddress": "0xd0a1e359811322d97991e03f863a0c30c2cf029c",
+    "takerTokenAddress": "0xcc2704ce33089d0f051eb0aff1750bb99fdfab46",
     "feeRecipient": "0xb046140686d052fff581f63f8136cce132e857da",
     "makerTokenAmount": "10000000000000000",
     "takerTokenAmount": "20000000000000000",
@@ -40,8 +41,8 @@ const orders = [
     "exchangeContractAddress": "0x12459c951127e0c374ff9105dda097662a027093",
     "maker": "0x9e56625509c2f60af937f23b7b532600390e8c8b",
     "taker": "0xa2b31dacf30a9c50ca473337c01d8a201ae33e32",
-    "makerTokenAddress": "0x323b5d4c32345ced77393b3530b1eed0f346429d",
-    "takerTokenAddress": "0xef7fff64389b814a946f3e92105513705ca6b990",
+    "makerTokenAddress": "0xd0a1e359811322d97991e03f863a0c30c2cf029c",
+    "takerTokenAddress": "0xcc2704ce33089d0f051eb0aff1750bb99fdfab46",
     "feeRecipient": "0xb046140686d052fff581f63f8136cce132e857da",
     "makerTokenAmount": "10000000000000000",
     "takerTokenAmount": "20000000000000000",
@@ -86,9 +87,9 @@ class ViewOrders extends Component {
     const orderNodes = orders.map(function (order, index) {
       return (
         <tr key={index}>
-          <td>{order.makerTokenAddress}</td>
+          <td>{getTokenNameFromAddress(order.makerTokenAddress)}</td>
           <td>{order.makerTokenAmount}</td>
-          <td>{order.takerTokenAddress}</td>
+          <td>{getTokenNameFromAddress(order.takerTokenAddress)}</td>
           <td>{order.takerTokenAmount} {order.costToken}</td>
           <td>
             <Button 
